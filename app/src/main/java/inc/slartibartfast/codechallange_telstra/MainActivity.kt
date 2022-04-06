683b286f2a74f334aa5e5de8255d6b222d9bd2f7
Slartibartfast <His.Royal.Slartness@gmail.com>
2022-04-06 14:01:48 +1000

    Implementing RecyclerView

--- app/src/main/java/inc/slartibartfast/codechallange_telstra/MainActivity.kt
+++ app/src/main/java/inc/slartibartfast/codechallange_telstra/MainActivity.kt
@@ -2,30 +2,38 @@ package inc.slartibartfast.codechallange_telstra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import inc.slartibartfast.codechallange_telstra.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+    val recyclerAdapter = TileAdapter()
     private lateinit var viewMode: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewMode = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        viewMode.makeApiCall()
+        initRecyclerView()
+        initViewModel()
+    }
 
-        viewMode.liveCountryData.observe(this) { countryData ->
-            val titles = StringBuilder()
-
-            title = countryData.title
-            countryData.rows.forEach {
-                titles.appendLine(it.title)
-            }
-
-            val textView = findViewById<TextView>(R.id.the_text_view)
-            textView.text = titles
+    private fun initRecyclerView() {
+        findViewById<RecyclerView>(R.id.recyclerView).apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = recyclerAdapter
         }
+    }
+
+    private fun initViewModel() {
+        viewMode = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewMode.liveCountryData.observe(this) {
+            title = it.title
+            recyclerAdapter.tileList = it.rows
+            recyclerAdapter.notifyDataSetChanged()
+        }
+
+        viewMode.makeApiCall()
     }
 }
